7ec9b309cf71fcce9bc36fdecdce8badf1490109
LUCVK1810 <lucuniversal101@gmail.com>
2024-09-15 16:41:48 +0530

    Add files via upload

---
+++ 6_A_Write a Python program to read an entire text file.py
@@ -0,0 +1,7 @@
+''' 
+Write a Python program to read an entire text file. 
+''' 
+def file_read(fname):
+    txt = open(fname)
+    print(txt.read()) 
+file_read('text.txt')
---
+++ 6_B_Write a Python program to append text to a file and display the text.py
@@ -0,0 +1,6 @@
+def main():
+    f=open("text.txt","a+")
+    f.write("Welcome to Workshop on Python")
+    f.close() 
+if __name__=="__main__":
+    main() 
